47cc157a53a706375c0dbb572cdba12c4593e6c1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-10 23:27:04 -0300

    feat: classe GerenciamentoVotacao e seus atributos

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,24 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class GerenciamentoVotacao {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfComputado;
+  private int totalVotos;
+
+  public GerenciamentoVotacao(
+      ArrayList<PessoaCandidata> pessoasCandidatas,
+      ArrayList<PessoaEleitora> pessoasEleitoras, 
+      ArrayList<String> cpfComputado,
+      int totalVotos
+  ) {
+    this.pessoasCandidatas = pessoasCandidatas;
+    this.pessoasEleitoras = pessoasEleitoras;
+    this.cpfComputado = cpfComputado;
+    this.totalVotos = totalVotos;
+  }
+  
 }
