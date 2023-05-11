a10558f1c65536347bbe754c17bf50d9de9933d3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-10 22:59:11 -0300

    feat: classe Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -1,5 +1,12 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class Pessoa {
+/**
+ * Classe abstrata que representa uma Pessoa.
+ */
+public abstract class Pessoa {
+  public String nome;
 
+  abstract String getNome();
+
+  abstract void setNome(String nome);
 }
