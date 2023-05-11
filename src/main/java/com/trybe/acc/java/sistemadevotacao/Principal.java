7d94519b0d6579700b8ad33e3983222570a8c1cc
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-10 21:38:23 -0300

    feat: método que cadastra candidato

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,10 +1,39 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  private static void perguntaSeQuerCadastrarCandidato() {
+    System.out.println("Cadastrar pessoa candidata?");
+    System.out.println("1 - Sim");
+    System.out.println("2 - Não");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+  }
 
+  private static void cadastraCandidato(Scanner scanner) {
+    perguntaSeQuerCadastrarCandidato();
+    short estaCadastrandoCandidato = scanner.nextShort();
+
+    while (estaCadastrandoCandidato == 1) {
+      System.out.println("Entre com o nome da pessoa candidata:");
+      String nomeDoCandidato = scanner.next();
+
+      System.out.println("Entre com o número da pessoa candidata:");
+      String numeroDoCandidato = scanner.next();
+
+      perguntaSeQuerCadastrarCandidato();
+      estaCadastrandoCandidato = scanner.nextShort();
+    }
+  }
+
+  public static void main(String[] args) {
+    GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
+    Scanner scanner = new Scanner(System.in);
+
+    cadastraCandidato(scanner);
+
+    scanner.close();
   }
 
 }
