7bf48520ed37e79f5b0c8145f5d1f7353054b5f5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-10 23:06:31 -0300

    feat: classe PessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,31 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+/**
+ * Classe que representa uma pessoa eleitora em um sistema de votação.
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  @Override
+  String getNome() {
+    return nome;
+  }
+
+  @Override
+  void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
